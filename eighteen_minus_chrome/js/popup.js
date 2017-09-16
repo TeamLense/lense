15b76f2f542acd807af880dd5b0b530c32a7c3c8
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-09-16 16:32:28 -0400

    finish UI logic

--- eighteen_minus_chrome/js/popup.js
+++ eighteen_minus_chrome/js/popup.js
@@ -1,36 +1,47 @@
 var savedData = {};
 
-function saveData(enabled) {
+function saveData(enabled, censor) {
   var data = {};
-  data['key'] = { enable: enabled };
+  data['key'] = { enable: enabled, censor: censor };
   chrome.storage.sync.set(data);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.sync.get('key', function(data) {
-      if (!chrome.runtime.error) 
-        savedData = data.key;
+  chrome.storage.sync.get('key', function(data) {
+    if (!chrome.runtime.error) 
+      savedData = data.key;
 
-      console.log(savedData);
+    var switchCheckbox = $('.switch-checkbox');
+    var statusText = $('p.status');
+    var filter = $('.filter');
+    var violence = $('#violence');
+    var sexual = $('#sexual');
 
-      var switchCheckbox = $('.switch-checkbox');
-      var statusText = $('p.status');
-      var filter = $('.filter');
-      if (savedData.enable) {
-        switchCheckbox.parent().addClass('active');
-        switchCheckbox.prop('checked', true);
-        statusText.html('<b>18-</b> running');
-        filter.slideDown();
-      }
+    if (savedData.enable) {
+      switchCheckbox.parent().addClass('active');
+      switchCheckbox.prop('checked', true);
+      statusText.html('<b>18-</b> running');
+      filter.slideDown();
+    }
 
-      switchCheckbox.parent().removeClass('hide');
+    if (savedData.censor && savedData.censor.violence) violence.prop('checked', true);
+    if (savedData.censor && savedData.censor.sexual) sexual.prop('checked', true);
 
-      switchCheckbox.click(function() {
-        $(this).parent().toggleClass('active');
-        filter.is(':visible') ? filter.slideUp() : filter.slideDown();
-        console.log('checked: ' + this.checked);
-        statusText.html(this.checked ? '<b>18-</b> running' : '<b>18-</b> sleeping');
-        saveData(this.checked);
-      });
+    switchCheckbox.parent().removeClass('hide');
+
+    function saveTotalData() {
+      saveData(switchCheckbox.is(':checked'), {violence: violence.is(':checked'), sexual: sexual.is(':checked')});
+    }
+
+    switchCheckbox.click(function() {
+      $(this).parent().toggleClass('active');
+      filter.is(':visible') ? filter.slideUp() : filter.slideDown();
+      statusText.html(this.checked ? '<b>18-</b> running' : '<b>18-</b> sleeping');
+      saveTotalData();
     });
+
+    filter.on('click', '#violence, #sexual', function() {
+      saveTotalData();
+    });
+  });
 });

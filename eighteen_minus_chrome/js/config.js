0bdda02475efea8a2d160e8a37f7b5c6b3c7fdcd
yifan <y454yang@gmail.com>
2017-09-17 07:11:55 -0400

    remove gitignore for convenience

---
+++ eighteen_minus_chrome/js/config.js
@@ -0,0 +1,7 @@
+const config = {
+    "url": "https://gateway-a.watsonplatform.net/visual-recognition/api",
+    "api_key": "c896536089fc9a40d6e8f4348125e95dbd1d1e69",
+    "classifiers": {
+      "violence": "Violence_1327604523"
+    }
+}

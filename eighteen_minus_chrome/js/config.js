5a7f926c6d79a7c0220ec8d6124f4edb50b1b84c
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-09-17 07:23:43 -0400

    fix merge

---
+++ eighteen_minus_chrome/js/config.js
@@ -0,0 +1,7 @@
+const config = {
+    "url": "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/",
+    "api_key": "f72f7c2a51b8ba214a3948b279914c80208a2311",
+    "classifiers": {
+      "violence": "Violence_1929902703"
+    }
+}

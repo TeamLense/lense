52f65b695b171604be5dfbdf5bfed4fd4eeb7db8
Daniel Zhou <danzhou1810@gmail.com>
2017-09-17 00:51:53 -0400

    using one endpoint for all classifiers

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -6,13 +6,14 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const databaseRef = admin.database().ref();
 
-app.post('/violence', (req, res) => {
+app.post('/new', (req, res) => {
+  let classifier = req.query.classifier;
   let class_name = req.body.class;
   let url = req.body.url;
   let url_hash = hashCode(url); // hashed int value as key
   let image_key;
 
-  let img_ref = databaseRef.child('violence/' + class_name + "/" + url_hash);
+  let img_ref = databaseRef.child(classifier + '/' + class_name + "/" + url_hash);
 
   img_ref.once('value').then(snapshot => {
     if (snapshot.val() != null) {
@@ -24,27 +25,6 @@ app.post('/violence', (req, res) => {
     }
     res.status(200).send("posting stuff to violence -> img: " + url + " of class: " + class_name);
   });
-});
-
-app.post('/sexual', (req, res) => {
-  let class_name = req.body.class;
-  let url = req.body.url;
-  let url_hash = hashCode(url); // hashed int value as key
-  let image_key;
-
-  let img_ref = databaseRef.child('sexual/' + class_name + "/" + url_hash);
-
-  img_ref.once('value').then(snapshot => {
-    if (snapshot.val() != null) {
-      console.log(snapshot.val());
-      img_ref.set({ url: url, report_count: snapshot.val().report_count + 1 })
-    } else {
-      console.log("empty image path");
-      img_ref.set( {url: url, report_count: 1 } );
-    }
-    res.status(200).send("posting stuff to sexual -> img: " + url + " of class: " + class_name);
-  });
-
 });
 
 hashCode = function(str){
